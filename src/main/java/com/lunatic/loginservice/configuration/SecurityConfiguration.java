781dbfa45e79a098fc3a34481dabfa7c53c6ba56
Prakash Gupta <prakash.25gupta@gmail.com>
2022-08-04 22:47:45 +0530

    Added Role based Authorization

--- src/main/java/com/lunatic/loginservice/configuration/SecurityConfiguration.java
+++ src/main/java/com/lunatic/loginservice/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.lunatic.loginservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,5 +26,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/admin").hasRole("ADMIN")
+                .antMatchers("/api/user").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 }
